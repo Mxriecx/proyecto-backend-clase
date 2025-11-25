d90bebba07d34a765a1281cff375ce322b56ea8b
Maria Paz <paztuiranmaria@gmail.com>
2025-11-24 21:32:49 -0500

    front en el back

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,58 +1,76 @@
-import { userModel } from "../models/users.model.js"
-import bcryptjs from "bcryptjs"
+import { userModel } from "../models/users.model.js";
+import bcryptjs from "bcryptjs";
 
 //definir las acciones que van a realizar - CRUD
 
 // 1. metodo para crear un producto -> Post
 
 export const postUser = async (request, response) => {
-    // aca va la logica de la peticion
-    try {
-        // deestructuracion se hace para procesar la informacion del usuario antes de guardarla 
+  // aca va la logica de la peticion
+  try {
+    // deestructuracion se hace para procesar la informacion del usuario antes de guardarla
 
-        const { name, age, username, password, role , email } = request.body;
+    const { name, age, username, password, role, email } = request.body;
 
-        const codedPassword = await bcryptjs.hash(password, 8);
+    const codedPassword = await bcryptjs.hash(password, 8);
 
-        await userModel.create({
-            name,
-            age,
-            username,
-            password: codedPassword,
-            role,
-            email
-        });
+    await userModel.create({
+      name,
+      age,
+      username,
+      password: codedPassword,
+      role,
+      email,
+    });
 
-        return response.status(201).json({
-            "mensaje": "usuario creado correctamente"
-        })
+    return response.status(201).json({
+      mensaje: "usuario creado correctamente",
+    });
+  } catch (error) {
+    return response.status(500).json({
+      mensaje: "Error al crear el usuario",
+      error: error.message || error,
+    });
+  }
+};
 
-    } catch (error) {
-        return response.status(500).json({
-            "mensaje": "Error al crear el usuario",
-            "error": error.message || error
+//2. metodo para mostrar todos los productos -> GET
 
-        })
+export const getUser = async (request, response) => {
+  try {
+    const allUsers = await userModel.find();
 
-    }
-
-}
-
-
-//2. metodo para mostrar todos los productos -> GET 
-
-export const getUser = (request, response) => {
-    return response.json({ "mensaje": "funciona peticion GET" })
-}
+    return response.status(200).json({
+      mensaje: "se encontraron todos los usuarios",
+      data: allUsers,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      mensaje: "ocurrio un error al mostrar los usuarios",
+      error: error.message || error,
+    });
+  }
+};
 
 //3. metodo para actualizar  un producto -> PUT
 
 export const putUserById = (request, response) => {
-    return response.json({ "mensaje": "funciona peticion PUT" })
-}
+  return response.json({ mensaje: "funciona peticion PUT" });
+};
 
 //4. metodo para Eliminar un producto -> DELETE
 
-export const deleteUserById = (request, response) => {
-    return response.json({ "mensaje": "funciona peticion DELETE" })
-} 
+export const deleteUserById = async (request, response) => {
+  try {
+    const idForDelete = request.params._id;
+    await userModel.findByIdAndDelete(idForDelete);
+    return response.status(200).json({
+      mensaje: "Usuario eliminado exitosamente",
+    });
+  } catch (error) {
+    return response.status(500).json({
+        "mensaje":"ocurrio un error al borrar el usuario",
+        "error":error.message || error
+    })
+};
+}
